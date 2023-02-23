8d908d9812792346acaa14d1c4a8a38dff9ac141
Tomas Garbarino <tgarbarino@apple.com>
2023-02-23 19:06:03 -0300

    css updates

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const iconMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+iconMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle('hidden');
+}
+
+function toggleMobileMenu( ){
+  mobileMenu.classList.toggle('hidden');
 }
